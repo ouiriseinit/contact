c65831de3c6dd92149e734491b06737577d2a0b8
ok <kalebkandake@gmail.com>
2025-12-30 11:09:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,30 @@ app.post('/api/send', async (req, res) => {
     res.status(500).json({ error: 'Failed to create user' });
   }
 })
-app.get('/api/send', (req, res) => res.redirect('https://ouiriseinit.github.io/'))
+app.get('/api/send', async (req, res) => {
+    try {
+    const { name, email, phone, message } = req.body;
+    // Extract the user data from req.body and create a new User instance
+    const newUser = new User({ name, email, phone });
+
+    // Save the new user to the database
+    await newUser.save();
+
+    const found = await User.findOne(newUser);
+    if (found) {
+        const newMessage = new Message({ name, user_id: found._id, message });
+        await newMessage.save();
+        res.redirect('https://ouiriseinit.github.io/')
+        //res.status(201).json({ message: 'User created successfully', userId: newUser.id });
+    }
+    // Send success response
+    
+  } catch (error) {
+    console.error('Error creating user:', error);
+    // Send error response
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+})
 
 app.get('/api/users', async (req, res) => {
     const result = await User.find({});
